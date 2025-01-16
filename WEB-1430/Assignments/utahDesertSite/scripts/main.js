feb77fc406296ec74fe11376c2d027e5626edc96
MysticBagel77902024 <mckayerickson@mail.weber.edu>
2025-01-16 11:37:17 -0700

    created repo, moved WEB 1430 files over

---
+++ WEB-1430/Assignments/utahDesertSite/scripts/main.js
@@ -0,0 +1,5 @@
+function makeRain(){
+	//alert("It's raining");
+	document.getElementById('conditions').innerHTML = "Current Conditions: Raining";
+	document.getElementById('pageBody').style = "background-color:#7E8C54;";
+}
